6d07fbb8e005b1dc1e430117ecdbbc36d7b238ea
R. Martinho Fernandes <rmf@rmf.io>
2025-07-20 12:36:31 +0200

    Manifest is a positional argument

--- src/args.rs
+++ src/args.rs
@@ -7,7 +7,7 @@ use crate::parse::Manifest;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    #[arg(short, long, default_value = "faust/faust.yml")]
+    #[arg(default_value = "faust/faust.yml")]
     pub manifest: Option<PathBuf>,
     #[arg(short, long, default_value = "faust/dist")]
     pub out_dir: Option<PathBuf>,
