35b26ad5d0ebe8aefeb5b5b203105d085909da1c
R. Martinho Fernandes <rmf@rmf.io>
2025-07-20 21:25:58 +0200

    Some unit card stuff

--- silphium/src/components/faction_roster.rs
+++ silphium/src/components/faction_roster.rs
@@ -2,7 +2,7 @@ use implicit_clone::unsync::IArray;
 use yew::prelude::*;
 use yew_autoprops::autoprops;
 
-use crate::model::Unit;
+use crate::model::{Discipline, Formation, Unit};
 
 #[autoprops]
 #[function_component(FactionRoster)]
@@ -28,12 +28,197 @@ pub fn faction_roster(roster: IArray<Unit>) -> Html {
 }
 
 #[autoprops]
+#[function_component(StaminaDetails)]
+pub fn stamina_details(unit: Unit) -> Html {
+    use std::fmt::Write as _;
+
+    let stamina = unit.stamina();
+    let mut title = if stamina == u32::MAX {
+        "Inexhaustible".into()
+    } else {
+        format!("{} stamina", stamina)
+    };
+    if unit.heat != 0 {
+        let _ = write!(
+            title,
+            "\nHeat {}: {:+}",
+            if unit.heat > 0 { "penalty" } else { "bonus" },
+            -unit.heat,
+        );
+    }
+
+    html! {
+      <div class="stamina" {title}>
+        if stamina > 0 {
+          <img src="/icons/attribute/stamina.svg" class="attribute" />
+          <span>{ stamina }</span>
+          if unit.heat != 0 {
+            <img src="/icons/attribute/heat.svg" class="attribute" />
+            <span>{ format!("{:+}", -unit.heat) }</span>
+          }
+        } else {
+          <img src="/icons/attribute/inexhaustible.svg" class="attribute" />
+        }
+      </div>
+    }
+}
+
+#[autoprops]
+#[function_component(TerrainDetails)]
+pub fn terrain_details(unit: Unit) -> Html {
+    use std::fmt::Write as _;
+
+    let mut title = "".to_string();
+    if unit.ground_bonus.scrub != 0 {
+        let _ = write!(title, "\n{:+}", unit.ground_bonus.scrub);
+    }
+    if unit.ground_bonus.forest != 0 {
+        let _ = write!(title, "\n{:+}", unit.ground_bonus.forest);
+    }
+    if unit.ground_bonus.sand != 0 {
+        let _ = write!(title, "\n{:+}", unit.ground_bonus.sand);
+    }
+    if unit.ground_bonus.snow != 0 {
+        let _ = write!(title, "\n{:+}", unit.ground_bonus.snow);
+    }
+    if title.len() > 0 {
+        title.remove(0);
+    }
+
+    html! {
+      <div class="terrain" {title}>
+        if unit.ground_bonus.scrub != 0 {
+          <div class="scrub">
+            if unit.ground_bonus.scrub > 0 {
+              <img class="attribute" src="/icons/terrain/scrub-down.svg" />
+            } else {
+              <img class="attribute" src="/icons/terrain/scrub-up.svg" />
+            }
+            <span>{ format!("{:+}", unit.ground_bonus.scrub) }</span>
+          </div>
+        }
+        if unit.ground_bonus.forest != 0 {
+          <div class="forest">
+            if unit.ground_bonus.forest > 0 {
+              <img class="attribute" src="/icons/terrain/forest-down.svg" />
+            } else {
+              <img class="attribute" src="/icons/terrain/forest-up.svg" />
+            }
+            <span>{ format!("{:+}", unit.ground_bonus.forest) }</span>
+          </div>
+        }
+        if unit.ground_bonus.sand != 0 {
+          <div class="sand">
+            if unit.ground_bonus.sand > 0 {
+              <img class="attribute" src="/icons/terrain/sand-down.svg" />
+            } else {
+              <img class="attribute" src="/icons/terrain/sand-up.svg" />
+            }
+            <span>{ format!("{:+}", unit.ground_bonus.sand) }</span>
+          </div>
+        }
+        if unit.ground_bonus.snow != 0 {
+          <div class="snow">
+            if unit.ground_bonus.snow > 0 {
+              <img class="attribute" src="/icons/terrain/snow-down.svg" />
+            } else {
+              <img class="attribute" src="/icons/terrain/snow-up.svg" />
+            }
+            <span>{ format!("{:+}", unit.ground_bonus.snow) }</span>
+          </div>
+        }
+      </div>
+    }
+}
+
+#[autoprops]
 #[function_component(UnitCard)]
 pub fn unit_card(unit: Unit) -> Html {
+    use std::fmt::Write as _;
+
+    let mut soldiers_title = format!("{} soldiers", unit.soldiers);
+    if unit.officers > 0 {
+        let _ = write!(
+            soldiers_title,
+            "\n{} {}",
+            unit.officers,
+            if unit.officers > 1 {
+                "officers"
+            } else {
+                "officer"
+            }
+        );
+    }
+
+    let formations = unit.formations.iter().map(|f| {
+        let title = match &f {
+            Formation::Square => "Square formation",
+            Formation::Horde => "Wedge formation",
+            Formation::Phalanx => "Phalanx",
+            Formation::Testudo => "Testudo",
+            Formation::Wedge => "Horde",
+            Formation::Schiltrom => "Schiltrom",
+            Formation::ShieldWall => "Shield wall",
+        };
+        html! {
+          <img src={format!("/icons/formation/{f}.svg")} {title} />
+        }
+    });
+
+    let discipline_tooltip = match unit.discipline {
+        Discipline::Low => "Low discipline",
+        Discipline::Normal => "Normal discipline",
+        Discipline::Disciplined => "Disciplined",
+        Discipline::Impetuous => "May charge without orders",
+        Discipline::Berserker => "Berserker",
+    };
+
     html! {
       <div class="unit-card">
-        <img src={unit.image} />
-        <span>{unit.name}</span>
+        <div class="name">{ unit.name.clone() }</div>
+        <div class="frame">
+          <img class="image" title={&unit.name} src={&unit.image} />
+          <div class="size-row">
+            <img class="icon" src="/icons/stat/soldiers.svg" title="Soldiers"            />
+            <div class="size" title={soldiers_title}>
+              <span class="soldiers">{ unit.soldiers }</span>
+              if unit.officers > 0 {
+                <span class="officers">{ unit.officers }</span>
+              }
+              <div class="formations">
+                {for formations}
+              </div>
+            </div>
+          </div>
+          <div class="cost-row">
+            <img class="icon" src="/icons/stat/cost.svg" title="Recruitment cost" />
+            <div class="cost" title={format!("Cost: {}", unit.cost)}>
+              <span>{ unit.cost }</span>
+              if unit.turns > 1 {
+                <div class="turns" title={format!("{} turns", unit.turns)}>
+                  <img class="attribute" src="/icons/attribute/turns.svg" />
+                  <span>{ unit.turns }</span>
+                </div>
+              }
+            </div>
+          </div>
+          <div class="upkeep-row">
+            <img class="icon" src="/icons/stat/upkeep.svg" title="Upkeep cost" />
+            <div class="upkeep" title={format!("Upkeep: {}", unit.upkeep)}>
+              <span>{ unit.upkeep }</span>
+            </div>
+          </div>
+          <div class="mental-row">
+            <img class="icon" src={format!("/icons/discipline/{}.svg", unit.discipline)} title={discipline_tooltip} />
+            <div class="mental" title={format!("Morale: {}", unit.morale)}>
+              <span class="morale">{ unit.morale }</span>
+              if unit.stamina() > 0 {
+                <StaminaDetails unit={&unit} />
+              }
+            </div>
+          </div>
+          <TerrainDetails unit={&unit} />
+        </div>
       </div>
     }
 }
--- silphium/src/model.rs
+++ silphium/src/model.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fmt::{self, Display, Formatter},
+};
 
 use implicit_clone::{
     ImplicitClone,
@@ -47,17 +50,17 @@ impl Faction {
 
 #[derive(PartialEq, Serialize, Deserialize, ImplicitClone, Clone, Debug)]
 pub struct Era {
-	pub id: IString,
+    pub id: IString,
     pub icon: IString,
     pub name: IString,
 }
 
 #[derive(PartialEq, Serialize, Deserialize, ImplicitClone, Clone, Debug)]
 pub struct Unit {
-	pub id: IString,
+    pub id: IString,
     pub name: IString,
     pub key: IString,
-	pub image: IString,
+    pub image: IString,
     pub soldiers: u32,
     pub officers: u32,
     pub attributes: IArray<Attr>,
@@ -78,6 +81,15 @@ pub struct Unit {
     pub eras: IArray<IString>,
 }
 
+impl Unit {
+    pub fn stamina(&self) -> u32 {
+        match self.attributes.iter().find(|a| matches!(a, Attr::Stamina(_))) {
+            Some(Attr::Stamina(s)) => s,
+            _ => 0,
+        }
+    }
+}
+
 #[derive(PartialEq, Serialize, Deserialize, ImplicitClone, Clone, Debug)]
 pub struct Weapon {
     pub factor: u32,
@@ -92,10 +104,10 @@ pub struct Weapon {
 #[derive(PartialEq, Serialize, Deserialize, ImplicitClone, Clone, Copy, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum WeaponType {
-	Melee,
-	Spear,
-	Missile,
-	Thrown,
+    Melee,
+    Spear,
+    Missile,
+    Thrown,
 }
 
 #[derive(PartialEq, Serialize, Deserialize, ImplicitClone, Clone, Copy, Debug)]
@@ -148,6 +160,20 @@ pub enum Formation {
     ShieldWall,
 }
 
+impl Display for Formation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Square => write!(f, "square"),
+            Self::Horde => write!(f, "horde"),
+            Self::Phalanx => write!(f, "phalanx"),
+            Self::Testudo => write!(f, "testudo"),
+            Self::Wedge => write!(f, "wedge"),
+            Self::Schiltrom => write!(f, "schiltrom"),
+            Self::ShieldWall => write!(f, "shield_wall"),
+        }
+    }
+}
+
 #[derive(PartialEq, Serialize, Deserialize, ImplicitClone, Clone, Copy, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum Discipline {
@@ -156,6 +182,18 @@ pub enum Discipline {
     Disciplined,
     Impetuous,
     Berserker,
+}
+
+impl Display for Discipline {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Low => write!(f, "low"),
+            Self::Normal => write!(f, "normal"),
+            Self::Disciplined => write!(f, "disciplined"),
+            Self::Impetuous => write!(f, "impetuous"),
+            Self::Berserker => write!(f, "berserker"),
+        }
+    }
 }
 
 #[derive(PartialEq, Serialize, Deserialize, ImplicitClone, Clone, Copy, Debug)]
